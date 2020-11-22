9b41e306c1cf477484dccf72bcc966306de4f092
Christine Dang <dangc101@gmail.com>
2020-11-22 18:59:54 -0500

    Videos embeded onto media page, event cards updated

--- temple-app/src/pages/media.js
+++ temple-app/src/pages/media.js
@@ -13,6 +13,74 @@ function media(){
         </div>
         <br></br>
 
+        <div className="row">
+            <div className="col-md-12">
+            <button onClick={('https://www.youtube.com/')} className="btn" id="youtube">YouTube Channel</button>
+            </div>
+
+        </div>
+
+            <div className="row">
+                <div className="col-md-4">
+                    <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+                        frameborder='0'
+                        allow='autoplay; encrypted-media'
+                        allowfullscreen
+                        title='video'
+                        id="videos"/>
+                </div>
+
+                <div className="col-md-4">
+                    <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+                        frameborder='0'
+                        allow='autoplay; encrypted-media'
+                        allowfullscreen
+                        title='video'
+                        id="videos"/>
+                </div>
+
+                <div className="col-md-4">
+                    <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+                        frameborder='0'
+                        allow='autoplay; encrypted-media'
+                        allowfullscreen
+                        title='video'
+                        id="videos"/>
+                </div>
+                
+            </div>
+
+            <div className="row">
+                <div className="col-md-4">
+                    <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+                        frameborder='0'
+                        allow='autoplay; encrypted-media'
+                        allowfullscreen
+                        title='video'
+                        id="videos"/>
+                </div>
+
+                <div className="col-md-4">
+                    <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+                        frameborder='0'
+                        allow='autoplay; encrypted-media'
+                        allowfullscreen
+                        title='video'
+                        id="videos"/>
+                </div>
+
+                <div className="col-md-4">
+                    <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+                        frameborder='0'
+                        allow='autoplay; encrypted-media'
+                        allowfullscreen
+                        title='video'
+                        id="videos"/>
+                </div>
+                
+            </div>
+
+
         </>
     )
     }
