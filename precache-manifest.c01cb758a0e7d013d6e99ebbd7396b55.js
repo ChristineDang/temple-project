28063998504194919ce353135343e3410b5c8719
Christine Dang <dangc101@gmail.com>
2021-08-10 18:57:34 -0400

    Updates

---
+++ precache-manifest.c01cb758a0e7d013d6e99ebbd7396b55.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f248041106d50514a048de5a4002bf98",
+    "url": "/temple-project/index.html"
+  },
+  {
+    "revision": "a4074dce16dc85b158d1",
+    "url": "/temple-project/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7f8b8031f1148d032f84",
+    "url": "/temple-project/static/css/main.38f6fadd.chunk.css"
+  },
+  {
+    "revision": "a4074dce16dc85b158d1",
+    "url": "/temple-project/static/js/2.d008d516.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/temple-project/static/js/2.d008d516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8b8031f1148d032f84",
+    "url": "/temple-project/static/js/main.531150ca.chunk.js"
+  },
+  {
+    "revision": "5f1732c66d0fff1c8c6f",
+    "url": "/temple-project/static/js/runtime-main.d21650e5.js"
+  },
+  {
+    "revision": "c3cee93329395482d4256f3ab0ec4268",
+    "url": "/temple-project/static/media/916549.c3cee933.png"
+  },
+  {
+    "revision": "9aac743708281ec9e66ac0abdaf5ce88",
+    "url": "/temple-project/static/media/IMG_0412.9aac7437.JPG"
+  },
+  {
+    "revision": "fd93aa1c28e08695022ba89aa6abce88",
+    "url": "/temple-project/static/media/IMG_0429.fd93aa1c.JPG"
+  },
+  {
+    "revision": "98e1f135841923ac329f1c0dfd7af4a1",
+    "url": "/temple-project/static/media/IMG_0466.98e1f135.JPG"
+  },
+  {
+    "revision": "24c2706a0a79adca15fc404cada94851",
+    "url": "/temple-project/static/media/IMG_0518.24c2706a.JPG"
+  },
+  {
+    "revision": "42b56e0649e4caf854b756abb2490e5f",
+    "url": "/temple-project/static/media/IMG_0696.42b56e06.JPG"
+  },
+  {
+    "revision": "65b111bcdfae2f6da3caed7d5795f701",
+    "url": "/temple-project/static/media/TMH.65b111bc.jpg"
+  },
+  {
+    "revision": "10457e68513739936a292b1b11bacf28",
+    "url": "/temple-project/static/media/biography.10457e68.jpg"
+  },
+  {
+    "revision": "993f1cedb3abe5b4c6a868a42efc9b56",
+    "url": "/temple-project/static/media/buddhaLight2.993f1ced.jpg"
+  },
+  {
+    "revision": "c2266fbd886377762cf2eb3cd178129a",
+    "url": "/temple-project/static/media/buddhaLight3.c2266fbd.jpg"
+  },
+  {
+    "revision": "2c4615fc15bffcc1ffd31839377d4081",
+    "url": "/temple-project/static/media/minhhieu.2c4615fc.png"
+  },
+  {
+    "revision": "b18e101a3bb3b71293b14cd77a856aec",
+    "url": "/temple-project/static/media/sydney3.b18e101a.jpg"
+  },
+  {
+    "revision": "4a857284367c7cf247155268774cc1b4",
+    "url": "/temple-project/static/media/tang ni 11.4a857284.JPG"
+  },
+  {
+    "revision": "de05c9a5d62bf38a553715cb879958a2",
+    "url": "/temple-project/static/media/tang ni 6.de05c9a5.JPG"
+  },
+  {
+    "revision": "6eb4492830ac6c76b5186a13308ba205",
+    "url": "/temple-project/static/media/tang ni 8.6eb44928.JPG"
+  },
+  {
+    "revision": "f0e487463b8514d7cf8aac6b3bce1712",
+    "url": "/temple-project/static/media/zenrocks.f0e48746.jpg"
+  }
+]);
